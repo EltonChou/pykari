b3ea9e8974e19246ab84cae76cb424064eed47fb
Elton Chou <plscd748@gmail.com>
2018-12-09 01:18:51 +0800

    PACAKGE_UPDATE:EorzeaEnv

--- gaijin.py
+++ gaijin.py
@@ -1,9 +1,10 @@
-import discord
 import logging
-from pymongo import MongoClient
-from bs4 import BeautifulSoup
-from EorzeaEnv import EorzeaWeather, EorzeaTime
 from datetime import datetime as dt
+
+import discord
+from bs4 import BeautifulSoup
+from EorzeaEnv import EorzeaTime, EorzeaWeather
+from pymongo import MongoClient
 from pytz import timezone
 
 # client = MongoClient("localhost", 27017)
@@ -16,7 +17,6 @@ class Gaijin(discord.Client):
         super().__init__(*args, **kwargs)
         self.__mongo_url = kwargs.get('mongo_url') or "localhost"
         self.__playing = discord.Game(name=kwargs.get('playing'))
-        self.ew = EorzeaWeather.EorzeaWeather()
         self.island_list = {
             "island1": "Eureka Anemos",
             "island2": "Eureka Pagos",
@@ -34,7 +34,7 @@ class Gaijin(discord.Client):
                 command = msg.content.split(' ')
 
                 if command[1] == 'weather':
-                    etl = EorzeaTime.EorzeaTime().next_weather_period_start()
+                    etl = EorzeaTime.next_weather_period_start()
                     try:
                         island = self.island_list[command[2]]
                     except KeyError as err:
@@ -45,16 +45,14 @@ class Gaijin(discord.Client):
                     if not island:
                         return
                     for time in etl:
-                        from_ = dt.fromtimestamp(time, tz=self.tw).strftime(
-                            "%H:%M")
-                        weather = self.ew.forecast_weather(island, time)
-                        weather = weather.lower().replace(" ", "_")
+                        from_ = dt.fromtimestamp(time, tz=self.tw).strftime("%H:%M")
+                        weather = EorzeaWeather.forecast_weather(island, time).lower().replace(" ", "_")
                         icon_id = discord.utils.find(
                             lambda e: e.name == weather, msg.server.emojis).id
                         result = '<:{}:{}> {}~'.format(weather, icon_id, from_)
                         results.append(result)
 
                     weather_transform = "\n".join(
-                        map(lambda e: str(e), results))
+                        str(r) for r in results)
 
                     await self.send_message(msg.channel, weather_transform)
